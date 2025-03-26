7390ee44941f479e6568208ea4f50f6fa8e4087e
Dave For Projects <daveforprojects@gmail.com>
2025-03-26 19:42:18 -0400

    Hello world

---
+++ HelloWorld/helloworld.ts
@@ -0,0 +1,7 @@
+// Source: https://code.visualstudio.com/docs/languages/typescript
+
+// To transpile: tsc helloworld.ts
+// To run js output: node helloworld.js
+
+let message: string = 'Hola, hola';
+console.log(message);
