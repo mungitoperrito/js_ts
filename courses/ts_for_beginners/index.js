08d75a31bcedbf9ff5faed96cf11bbb990723950
Dave For Projects <daveforprojects@gmail.com>
2025-03-27 11:20:31 -0400

    Tutorial

---
+++ courses/ts_for_beginners/index.js
@@ -0,0 +1,3 @@
+// Ex 01
+console.log('Hola, hola'); // Transpiles to the same code
+var age = 20; // Transpiles to the type-free code
---
+++ courses/ts_for_beginners/index.ts
@@ -0,0 +1,6 @@
+// Transpile and run if ok: tsc index.ts  && node index.js
+
+
+// Ex 01
+console.log('Hola, hola');        // Transpiles to the same code
+let age: number = 20;             // Transpiles to the type-free code
