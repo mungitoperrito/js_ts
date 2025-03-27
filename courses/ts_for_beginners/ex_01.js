b91a70747730ad2288cea26713548434f0a5d2af
Dave Cuthbert <dave.cuthbert@escocimex.net>
2025-03-27 11:24:05 -0400

    Tutorial (#3)
    * Tutorial
    
    * Tutorial

---
+++ courses/ts_for_beginners/ex_01.js
@@ -0,0 +1,4 @@
+// Transpile and run if ok: tsc index.ts  && node ex_01.js
+// Ex 01
+console.log('Hola, hola'); // .js is the same code
+var age = 20; // .js is type-free code, var not let
---
+++ courses/ts_for_beginners/ex_01.ts
@@ -0,0 +1,6 @@
+// Transpile and run if ok: tsc index.ts  && node ex_01.js
+
+
+// Ex 01
+console.log('Hola, hola');        // .js is the same code
+let age: number = 20;             // .js is type-free code, var not let
