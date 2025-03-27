fb7e03d8dde8c6c8a5056f0f75a5713df10a6cb6
Dave For Projects <daveforprojects@gmail.com>
2025-03-27 11:44:41 -0400

    tsconfig

--- courses/ts_for_beginners/ex_01.js
+++
@@ -1,4 +0,0 @@
-// Transpile and run if ok: tsc index.ts  && node ex_01.js
-// Ex 01
-console.log('Hola, hola'); // .js is the same code
-var age = 20; // .js is type-free code, var not let
---
+++ courses/ts_for_beginners/src/ex_02.ts
@@ -0,0 +1,11 @@
+// To run code: tsc && ex_0t.js
+
+// Config the ts compiler
+// To create tsconfig.json:  tsc --init
+//    target - lower number, better browser support
+//    rootDir - source location
+//    outDir  - transpiled output
+//    removeComments - shorter output
+//    oEmitOnError - only write js if transpile succeeds
+
+console.log("EX 02")
