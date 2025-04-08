f29aecbadac1c5769ab83714639f0b43b0f0e13b
Dave Cuthbert <daveforprojects@gmail.com>
2025-04-08 16:54:26 -0400

    Live server (#15)

---
+++ courses/oop_in_js/ex02.js
@@ -0,0 +1,6 @@
+// Open dev tools and check in the console tab
+console.log("Hola ex02.js");
+
+
+// Visible in browser directly
+document.getElementById("pageDisplay01").innerHTML = "<h2>On the page</h2>";
