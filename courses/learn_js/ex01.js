817809ea574cfbf987a78b05dbf7479e76cebab6
Dave Cuthbert <daveforprojects@gmail.com>
2025-04-14 05:19:13 -0400

    notes (#25)
    * js01
    
    * js01
    
    * notes

---
+++ courses/learn_js/ex01.js
@@ -0,0 +1,37 @@
+// Syntax notes
+
+let variable = 10;
+// // Increment, decrement
+// console.log(variable);
+// variable++;
+// console.log(variable);
+// variable--;
+// console.log(variable);
+
+// // Add, subtract, multiply, divide
+// variable +=10;
+// console.log(variable);
+// variable -=10;
+// console.log(variable);
+// variable *=10;
+// console.log(variable);
+// variable /=10;
+// console.log(variable);
+
+
+// // String mutability
+// let str01 = 'B short string';
+// console.log("str01: ", str01);
+
+// str01[0] = 'A';                     // Character is not updated
+// console.log("str01[0]: ", str01);
+
+// str01 = 'A short string'            // Whole string is replaced
+// console.log("str01: ", str01);
+
+// String positions
+let str01 = 'a bc def';
+console.log(str01);
+console.log('Last: ', str01[str01.length -1]);
+console.log('Minus: ', str01[-1]);           // Doesn't work like Python
+console.log();
